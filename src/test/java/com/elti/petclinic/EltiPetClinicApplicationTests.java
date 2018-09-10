2a878170fe3aba745aec764d2170a3a027dece2f
Elti <elti.shehu.sp@lhind.dlh.de>
2018-09-10 12:18:12 +0200

    Initial Project Add

---
+++ src/test/java/com/elti/petclinic/EltiPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elti.petclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class) @SpringBootTest public class EltiPetClinicApplicationTests {
+
+	@Test public void contextLoads() {
+	}
+
+}
