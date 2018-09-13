c99ed2d72e1344df8194344411bbc660a4de63ec
Elti <elti.shehu.sp@lhind.dlh.de>
2018-09-13 09:40:47 +0200

    Refactoring to multimodel maven project. Closes #1.

--- pet-clinic-web/src/main/java/com/elti/petclinic/EltiPetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/elti/petclinic/EltiPetClinicApplication.java
@@ -3,7 +3,8 @@ package com.elti.petclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication public class EltiPetClinicApplication {
+@SpringBootApplication
+public class EltiPetClinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EltiPetClinicApplication.class, args);
--- pet-clinic-web/src/test/java/petclinic/EltiPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/petclinic/EltiPetClinicApplicationTests.java
@@ -1,4 +1,4 @@
-package com.elti.petclinic;
+package petclinic;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
