8550d4883a998284082105ea2a0474063a60679b
Elti <elti.shehu.sp@lhind.dlh.de>
2018-09-13 09:45:51 +0200

    Fixed failing test.

--- pet-clinic-web/src/test/java/com/elti/petclinic/EltiPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/elti/petclinic/EltiPetClinicApplicationTests.java
@@ -1,13 +1,16 @@
-package petclinic;
+package com.elti.petclinic;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class) @SpringBootTest public class EltiPetClinicApplicationTests {
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EltiPetClinicApplicationTests {
 
-	@Test public void contextLoads() {
+	@Test
+	public void contextLoads() {
 	}
 
 }
