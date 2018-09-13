dcc7247b108c1dfde244eaae4973c1e50d3c415d
Elti <elti.shehu.sp@lhind.dlh.de>
2018-09-13 09:15:10 +0200

    Adding POJOs. Closes #2.

---
+++ src/main/java/com/elti/petclinic/model/Owner.java
@@ -0,0 +1,7 @@
+package com.elti.petclinic.model;
+
+/**
+ * Created by e.sh. on 13-Sep-18.
+ */
+public class Owner extends Person {
+}
---
+++ src/main/java/com/elti/petclinic/model/PetType.java
@@ -0,0 +1,17 @@
+package com.elti.petclinic.model;
+
+/**
+ * Created by e.sh. on 13-Sep-18.
+ */
+public class PetType {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
