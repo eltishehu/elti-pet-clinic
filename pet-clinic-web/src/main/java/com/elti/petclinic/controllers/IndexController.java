a712f0060cac6fbf1fac46eb5e09d6d9d5ebe9af
Elti Shehu <shehuelti@gmail.com>
2018-09-26 16:09:15 +0200

    Added Index page and controller. Closes #8.

---
+++ pet-clinic-web/src/main/java/com/elti/petclinic/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.elti.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by e.sh. on 26-Sep-18
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
