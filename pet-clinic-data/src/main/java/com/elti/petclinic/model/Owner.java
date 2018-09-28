4f47179775c4901d8a76c6891f700239c2f8adf0
Elti Shehu <shehuelti@gmail.com>
2018-09-28 10:38:13 +0200

    Added Visit entity, updated Owner with Pet relationship. Closes #26.

--- pet-clinic-data/src/main/java/com/elti/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/elti/petclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package com.elti.petclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by e.sh. on 13-Sep-18.
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
