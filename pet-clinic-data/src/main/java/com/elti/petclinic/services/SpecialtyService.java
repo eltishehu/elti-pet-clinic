8721aa77b42f33dc0a67a79be75907181c4cd56a
Elti Shehu <shehuelti@gmail.com>
2018-09-28 12:24:23 +0200

    Added Specialty Map Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/com/elti/petclinic/services/SpecialtyService.java
@@ -0,0 +1,10 @@
+package com.elti.petclinic.services;
+
+import com.elti.petclinic.model.Specialty;
+
+/**
+ * Created by e.sh. on 28-Sep-18
+ */
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/elti/petclinic/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,39 @@
+package com.elti.petclinic.services.map;
+
+import com.elti.petclinic.model.Specialty;
+import com.elti.petclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by e.sh. on 28-Sep-18
+ */
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty specialty) {
+        return super.save(specialty);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty specialty) {
+        super.delete(specialty);
+    }
+}
