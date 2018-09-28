99020d18eede7ebab58cdeabc7649287dc2be873
Elti Shehu <shehuelti@gmail.com>
2018-09-28 11:49:20 +0200

    Added PetTypes to bootstrap startup class. Closes #32.

--- pet-clinic-web/src/main/java/com/elti/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/elti/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.elti.petclinic.bootstrap;
 
 import com.elti.petclinic.model.Owner;
+import com.elti.petclinic.model.PetType;
 import com.elti.petclinic.model.Vet;
 import com.elti.petclinic.services.OwnerService;
+import com.elti.petclinic.services.PetTypeService;
 import com.elti.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -15,14 +17,26 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedatPetType = petTypeService.save(cat);
+
+        System.out.println("Loaded PetTypes...");
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
