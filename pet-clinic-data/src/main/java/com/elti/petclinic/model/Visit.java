b62987f72eb753303fc73e0dedd3d5369e813d22
Elti Shehu <shehuelti@gmail.com>
2018-10-01 15:41:29 +0200

    Updated Visit entity with JPA entity mappings. Closes #38.

--- pet-clinic-data/src/main/java/com/elti/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/elti/petclinic/model/Visit.java
@@ -1,16 +1,23 @@
 package com.elti.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by e.sh. on 28-Sep-18
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
 
+    @Column(name = "description")
     private String description;
 
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
