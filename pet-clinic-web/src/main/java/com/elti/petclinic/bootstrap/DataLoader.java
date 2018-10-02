0c050bb526317ef77ca1cf679c9ec2eb124ee505
Elti Shehu <shehuelti@gmail.com>
2018-10-02 09:44:28 +0200

    Implemented JPA Visit Service. Added loader data. Closes #47.

--- pet-clinic-web/src/main/java/com/elti/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/elti/petclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.elti.petclinic.bootstrap;
 
 import com.elti.petclinic.model.*;
-import com.elti.petclinic.services.OwnerService;
-import com.elti.petclinic.services.PetTypeService;
-import com.elti.petclinic.services.SpecialtyService;
-import com.elti.petclinic.services.VetService;
+import com.elti.petclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -20,12 +17,14 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -106,6 +105,13 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+        visitService.save(catVisit);
 
         // =========================================================
 
